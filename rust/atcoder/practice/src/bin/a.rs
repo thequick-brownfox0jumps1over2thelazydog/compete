65946e9fe01bf8ce12f528c8811fa398c4b246ea
thequick-brownfox0jumps1over2thelazydog <161028349+thequick-brownfox0jumps1over2thelazydog@users.noreply.github.com>
2023-09-25 12:59:07 +0900

    pass A test

--- rust/atcoder/practice/src/bin/a.rs
+++ rust/atcoder/practice/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u16,
+        b: u16,
+        c: u16,
+        s: String,
+    }
+
+    println!("{} {}", a + b + c, s);
 }
